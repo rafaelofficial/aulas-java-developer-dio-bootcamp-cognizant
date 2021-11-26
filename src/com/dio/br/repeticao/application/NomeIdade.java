2373234524357dcc37c718cc158ceb377ea69549
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-11-26 16:17:00 -0300

    Exercício 1 - Nome e Idade

---
+++ src/com/dio/br/repeticao/application/NomeIdade.java
@@ -0,0 +1,28 @@
+package com.dio.br.repeticao.application;
+
+import java.util.Scanner;
+
+/**
+ * Faça um programa que leia conjuntos de dois valores,
+ * o primeiro represemtadp o nome do aluno e o segundo representando a sua idade.
+ * (Pare o programa inserindo o valor 0 no campo nome).
+ */
+public class NomeIdade {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while (true) {
+            System.out.println("Nome: ");
+            nome = sc.next();
+            if (nome.equals("0")) break;
+            System.out.println("Idade: ");
+            idade = sc.nextInt();
+        }
+        System.out.println("Continua aqui...");
+    }
+}
