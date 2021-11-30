38288e516ead68633b7003ab182a07b4bdda54bc
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-11-30 17:15:03 -0300

    Exercício 7 - Ordem inversa

---
+++ src/com/dio/br/arrays/application/OrdemInversa.java
@@ -0,0 +1,25 @@
+package com.dio.br.arrays.application;
+
+import java.util.Scanner;
+
+/**
+ * Crie um vetor de 6 números e mostre-os na ordem inversa.
+ */
+public class OrdemInversa {
+    public static void main(String[] args) {
+
+        int[] vetor = {-5, -6, 15, 50, 8, 4};
+
+        System.out.print("Vetor: ");
+        int contador = 0;
+        while (contador <= (vetor.length - 1)) {
+            System.out.print(vetor[contador] + " ");
+            contador++;
+        }
+
+        System.out.print("\nVetor: ");
+        for (int i = (vetor.length -1); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
