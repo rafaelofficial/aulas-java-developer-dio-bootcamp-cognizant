6096de9305d79ba5fe6e1ec106f25a28bbbefdf0
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-11-30 18:59:48 -0300

    Exercício 10 - Arrays Multidimencionais

---
+++ src/com/dio/br/arrays/application/ArrayMultidimensional.java
@@ -0,0 +1,28 @@
+package com.dio.br.arrays.application;
+
+import java.util.Random;
+
+/**
+ * Gere e imprima uma matriz M 4x4 com valores aleatórios entre 0 e 9.
+ */
+public class ArrayMultidimensional {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[][] M = new int[4][4];
+
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M.length; j++) {
+                M[i][j] = random.nextInt(9);
+            }
+        }
+        System.out.println("MATRIZ:");
+        for (int[] linha : M) {
+            for (int coluna : linha) {
+                System.out.print(coluna + " ");
+            }
+            System.out.println();
+        }
+    }
+}
