e178e963d62c1945c301de261a2650129d86579f
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-11-30 18:41:48 -0300

    Exercício 9 - Números Aleatórios

---
+++ src/com/dio/br/arrays/application/NumerosAleatorios.java
@@ -0,0 +1,38 @@
+package com.dio.br.arrays.application;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Faça um programa que leia 20 números inteiros aleatórios (entre 0 e 100), armazene-os num vetor.
+ * Ao final mostre os números e seus sucessores.
+ */
+public class NumerosAleatorios {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.println("NÚMEROS ALEATÓRIOS:");
+        for (int numero : numerosAleatorios) {
+            System.out.print(numero + " ");
+        }
+
+        System.out.println("\nANTECESSOR DOS NÚMEROS ALEATÓRIOS:");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero - 1) + " ");
+        }
+
+        System.out.println("\nSUCESSORES DOS NÚMEROS ALEATÓRIOS:");
+        for (int numero : numerosAleatorios) {
+            System.out.print((numero + 1) + " ");
+        }
+
+    }
+}
