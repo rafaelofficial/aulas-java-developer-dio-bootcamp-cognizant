ee1cf919a8fac1afbee88a168f4c7e5bbe235a37
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-12-23 17:24:16 -0300

    Implementação da classe ListaCircular (Métodos: getNo e get)

--- src/main/java/com/dio/br/estrutura/de/dados/application/listaCircular/src/main/java/com/dio/br/ListaCircular.java
+++ src/main/java/com/dio/br/estrutura/de/dados/application/listaCircular/src/main/java/com/dio/br/ListaCircular.java
@@ -3,8 +3,32 @@ package com.dio.br;
 public class ListaCircular<T> {
 
     private No<T> cabeca;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
+
+    /**
+     * @param index busca o elemento passado por parâmetro
+     * @return retorna o conteúdo pego da lista
+     */
+    public T get(int index) {
+        return this.getNo(index).getConteudo();
+    }
+
+    /**
+     * Método auxiliar
+     * @param index pega o índice da lista
+     * @return retorna o elemento da cauda da lista
+     */
+    private No<T> getNo(int index) {
+        if (this.isEmpty()) throw new IndexOutOfBoundsException("A lista está vazia.");
+        if (index == 0) return this.cauda;
+
+        No<T> noAuxiliar = this.cauda;
+        for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
 
     /**
      * testa se a lista está vazia ou não
@@ -20,6 +44,5 @@ public class ListaCircular<T> {
     public int size() {
         return this.tamanhoLista;
     }
-
 
 }
