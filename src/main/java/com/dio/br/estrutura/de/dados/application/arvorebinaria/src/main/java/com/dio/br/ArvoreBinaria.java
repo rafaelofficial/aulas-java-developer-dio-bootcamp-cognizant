9b69fbdfb8bd23a27f195ab179fb0c905c31a866
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2021-12-28 18:33:20 -0300

    Implementação Classe ArvoreBinaria: Métodos exibeEmOrdem, exibePosOrdem e exibePreOrdem

--- src/main/java/com/dio/br/estrutura/de/dados/application/arvorebinaria/src/main/java/com/dio/br/ArvoreBinaria.java
+++ src/main/java/com/dio/br/estrutura/de/dados/application/arvorebinaria/src/main/java/com/dio/br/ArvoreBinaria.java
@@ -9,7 +9,6 @@ public class ArvoreBinaria<T extends Comparable<T>> {
     }
 
     /**
-     * Método auxiliar
      * @param conteudo recebe o conteúdo de um nó
      */
     public void inserir(T conteudo) {
@@ -18,7 +17,7 @@ public class ArvoreBinaria<T extends Comparable<T>> {
     }
 
     /**
-     * @param atual se for nulo, o nó atual estará na raiz
+     * @param atual se for nulo, o nó atual estará na raiz, senão será comparado com o novoNo e se for menor do que 0 irá para a esqueda, senão para a direita.
      * @param novoNo passa a ser a raiz
      * @return retorna o nó atual
      */
@@ -33,4 +32,58 @@ public class ArvoreBinaria<T extends Comparable<T>> {
         return atual;
     }
 
+    /**
+     * Exibe em ordem
+     */
+    public void exibirEmOrdem() {
+        System.out.println("\nExibindo em ordem...");
+        exibirEmOrdem(this.raiz);
+    }
+
+    /**
+     * @param atual se caso não estiver nulo, o nó atual verifica a esquerda e exibe o conteúdo da raiz e posteriormente verifica a direita.
+     */
+    private void exibirEmOrdem(NoBinario<T> atual) {
+        if (atual != null) {
+            exibirEmOrdem(atual.getNoEsquerdo());
+            System.out.println(atual.getConteudo() + ", ");
+            exibirEmOrdem(atual.getNoDireito());
+        }
+    }
+
+    /**
+     * Exibe pós ordem
+     */
+    public void exibirPosOrdem() {
+        System.out.println("\nExibindo pós ordem...");
+        exibirEmOrdem(this.raiz);
+    }
+
+    /**
+     * @param atual se caso não estiver nulo, o nó atual verifica a esqyerda, posteriormente verifica a direita e exibe o conteúdo da raiz.
+     */
+    private void exibePosOrdem(NoBinario<T> atual) {
+        exibirEmOrdem(atual.getNoEsquerdo());
+        exibirEmOrdem(atual.getNoDireito());
+        System.out.println(atual.getConteudo() + ", ");
+    }
+
+    /**
+     * Exibe pré ordem
+     */
+    public void exibePreOrdem() {
+        System.out.println("\nExibindo pré ordem...");
+        exibePreOrdem(this.raiz);
+    }
+
+    /**
+     * @param atual se caso não estiver nulo, exibe o conteúdo da raiz, o nó atual verifica a esqyerda, posteriormente verifica a direita
+     */
+    private void exibePreOrdem(NoBinario<T> atual) {
+        if (atual != null) {
+            System.out.println(atual.getConteudo() + ", ");
+            exibePreOrdem(atual.getNoEsquerdo());
+            exibePreOrdem(atual.getNoDireito());
+        }
+    }
 }
