4d7bececdae85d46ef0b75b66e71e6a887724e5e
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-01-29 17:44:45 -0300

    Trabalhando com Mocks

--- src/main/java/com/dio/br/qa-testes/src/test/java/com/teste/CalculadoraTest.java
+++ src/main/java/com/dio/br/qa-testes/src/test/java/com/teste/CalculadoraTest.java
@@ -1,7 +1,10 @@
 package com.teste;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculadoraTest {
 
@@ -9,6 +12,16 @@ public class CalculadoraTest {
     public void testSomar() {
         Calculadora calculadora = new Calculadora();
         int soma = calculadora.somar("1+1+3");
-        Assert.assertEquals(5, soma);
+        assertEquals(5, soma);
+    }
+
+    @Test
+    public void testSomarComMock() {
+        Calculadora calculadora = mock(Calculadora.class);
+
+        when(calculadora.somar("1+2")).thenReturn(10);
+
+        int resultado = calculadora.somar("1+2");
+        assertEquals(10, resultado);
     }
 }
