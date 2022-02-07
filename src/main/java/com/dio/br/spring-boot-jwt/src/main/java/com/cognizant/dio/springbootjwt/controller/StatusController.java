825a86c3d76400cba7ceab42e7240ed00b0dc01e
rafaelofficial <rafael.sousa.pereira@mail.usf.edu.br>
2022-02-07 03:42:18 -0300

    Desenvolvendo o pacote de controller/Primeiros testes de autenticação de usuário

---
+++ src/main/java/com/dio/br/spring-boot-jwt/src/main/java/com/cognizant/dio/springbootjwt/controller/StatusController.java
@@ -0,0 +1,13 @@
+package com.cognizant.dio.springbootjwt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @RequestMapping("/status")
+    public String viewStatus() {
+        return "online";
+    }
+}
---
+++ src/main/java/com/dio/br/spring-boot-jwt/src/main/java/com/cognizant/dio/springbootjwt/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.cognizant.dio.springbootjwt.controller;
+
+import com.cognizant.dio.springbootjwt.data.UserData;
+import com.cognizant.dio.springbootjwt.service.UserDetailsServiceImpl;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public UserController(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @RequestMapping("/all-users")
+    public List<UserData> listAllUsers() {
+        return userDetailsService.listUsers();
+    }
+}
